81b84f955193ab5ff3a11b83f51f3913257ca2d6
Gabriele Di Bari <dbgabri@gmail.com>
2014-06-13 14:13:43 +0200

    Added physics debug  draw

--- include/World.h
+++ include/World.h
@@ -11,12 +11,24 @@ namespace Easy2D
 /////////////////////
 class Body;
 /////////////////////
+class GLDebugDraw : public b2Draw
+{
+    public:
+    virtual void DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
+    virtual void DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color);
+    virtual void DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color);
+    virtual void DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color);
+    virtual void DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color);
+    virtual void DrawTransform(const b2Transform& xf);
+};
+/////////////////////
 class World
 {
 
     friend class Body;
     b2World *world;
     b2BlockAllocator blockAllocator;
+    GLDebugDraw      debugDraw;
 
 public:
 
@@ -26,7 +38,8 @@ public:
     void  deletePhysics(Object* obj);
     void  setGravity(const Vec2& gravity);
     void  physics(float dt, uint velocityIterations=8, uint positionIterations=3);
-
+    void  enableDebugDraw();
+    void  physicsDraw();
 };
 
 
--- src/World.cpp
+++ src/World.cpp
@@ -28,7 +28,148 @@ void World::addPhysics(Object* obj)
 {
     obj->physics.registerWorld(this);
 }
-void  World::deletePhysics(Object* obj)
+void World::deletePhysics(Object* obj)
 {
     obj->physics.unregisterWorld(this);
 }
+void World::enableDebugDraw()
+{
+    debugDraw.SetFlags(b2Draw::e_shapeBit|
+                       b2Draw::e_jointBit|
+                       b2Draw::e_aabbBit|
+                       b2Draw::e_pairBit|
+                       b2Draw::e_centerOfMassBit);
+    world->SetDebugDraw(&debugDraw);
+}
+void World::physicsDraw()
+{   
+    //set projection matrix 
+    //NOTE: GET CAMERA, VIEWPORT
+    /*
+    Vec4 viewport;
+    glGetFloatv(GL_VIEWPORT, viewport);
+    glMatrixMode(GL_PROJECTION);
+    Mat4 projection;
+    projection.setOrtho(viewport.x,viewport.z, viewport.y,viewport.w, 1.0f,-1.0f);
+    glLoadMatrixf(projection);
+    //set view port
+    //glViewport( viewport.x, viewport.y, (GLsizei)viewport.z, (GLsizei)viewport.w );
+    */
+    //GET pos camera
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //no vbo/ibo
+    glBindBuffer( GL_ARRAY_BUFFER, 0 );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER,  0 );
+    //no texture
+    glDisable( GL_TEXTURE_2D );
+    world->DrawDebugData();
+}
+
+////////////////////////////////////////
+#define SAVE_COLOR\
+    GLfloat saveGLColor4f[4];\
+    glGetFloatv(GL_CURRENT_COLOR, saveGLColor4f);
+#define RESET_COLOR\
+    glColor4fv(saveGLColor4f);
+
+void GLDebugDraw::DrawPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color) 
+{
+    SAVE_COLOR
+    glColor4f(color.r, color.g, color.b,1);
+    glVertexPointer(2, GL_FLOAT, 0, vertices);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
+    RESET_COLOR
+}
+void GLDebugDraw::DrawSolidPolygon(const b2Vec2* vertices, int32 vertexCount, const b2Color& color)
+{
+    SAVE_COLOR
+
+    glVertexPointer(2, GL_FLOAT, 0, vertices);
+
+    glColor4f(color.r, color.g, color.b,0.5f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, vertexCount);
+
+    glColor4f(color.r, color.g, color.b,1);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
+    
+    RESET_COLOR
+}
+void GLDebugDraw::DrawCircle(const b2Vec2& center, float32 radius, const b2Color& color) 
+{
+    SAVE_COLOR
+
+    const float32 k_segments = 16.0f;
+    const int vertexCount=16;
+    const float32 k_increment = 2.0f * b2_pi / k_segments;
+    float32 theta = 0.0f;
+    
+    Vec2 vertices[vertexCount];
+
+    for (int32 i = 0; i < k_segments; ++i)
+    {
+        vertices[i]= cast(center) + radius * Vec2(cosf(theta), sinf(theta));
+        theta += k_increment;
+    }
+    
+    glVertexPointer(2, GL_FLOAT, 0, vertices);
+
+    glColor4f(color.r, color.g, color.b,1);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
+
+    RESET_COLOR
+}
+void GLDebugDraw::DrawSolidCircle(const b2Vec2& center, float32 radius, const b2Vec2& axis, const b2Color& color)
+{
+    SAVE_COLOR
+
+    const float32 k_segments = 16.0f;
+    const int vertexCount=16;
+    const float32 k_increment = 2.0f * b2_pi / k_segments;
+    float32 theta = 0.0f;
+
+    Vec2 vertices[vertexCount];
+
+    for (int32 i = 0; i < k_segments; ++i)
+    {
+        vertices[i]= cast(center) + radius * Vec2(cosf(theta), sinf(theta));
+        theta += k_increment;
+    }
+    
+    glVertexPointer(2, GL_FLOAT, 0, vertices);
+
+    glColor4f(color.r, color.g, color.b,0.5f);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, vertexCount);
+
+    glColor4f(color.r, color.g, color.b,1);
+    glDrawArrays(GL_LINE_LOOP, 0, vertexCount);
+    
+    RESET_COLOR
+}
+void GLDebugDraw::DrawSegment(const b2Vec2& p1, const b2Vec2& p2, const b2Color& color)
+{
+    SAVE_COLOR
+
+    GLfloat vertices[] = 
+    {
+        p1.x,p1.y,p2.x,p2.y
+    };
+    glColor4f(color.r, color.g, color.b,1);
+    glVertexPointer(2, GL_FLOAT, 0, vertices);
+    glDrawArrays(GL_LINES, 0, 2);
+
+    RESET_COLOR
+}
+void GLDebugDraw::DrawTransform(const b2Transform& xf)
+{
+    b2Vec2 p1 = xf.p;
+    b2Vec2 p2;
+    const float32 k_axisScale = 0.4f;
+
+    p2 = p1 + k_axisScale * xf.q.GetXAxis();
+    DrawSegment(p1,p2,b2Color(1,0,0));
+
+    p2 = p1 + k_axisScale * xf.q.GetYAxis();
+    DrawSegment(p1,p2,b2Color(0,1,0));
+}
+
