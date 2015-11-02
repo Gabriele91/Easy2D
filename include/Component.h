7bb4239476d1971fa7b759a63c26eac27c0e7d8a
Gabriele Di Bari <dbgabri@gmail.com>
2015-11-02 21:32:04 +0100

    Fixed a wrong

--- include/Component.h
+++ include/Component.h
@@ -14,7 +14,7 @@ namespace Easy2D {
     template<class T>
     class ComponentItem;
     /////////////////////
-    class E2DDLL Component : public AlignedAlloc<16>
+    class Component : public AlignedAlloc<16>
     {
 
         //friend class
